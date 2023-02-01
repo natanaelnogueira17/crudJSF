11911797c10bdf18484da682cab4a53c9752e7e8
natanael nogueira <natanaelnogueira019@gmail.com>
2023-02-01 17:03:25 -0300

    criando o projeto

---
+++ src/main/java/JSFeMaven/CRUDJSF/PessoaBean.java
@@ -0,0 +1,29 @@
+package JSFeMaven.CRUDJSF;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class PessoaBean  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private String nome;
+	private String sobrenome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+
+}
